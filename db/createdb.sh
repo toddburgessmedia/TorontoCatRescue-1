47ff78af5746e55fcd810965af2b7882bb49d710
Todd Burgess <todd@toddburgessmedia.com>
2019-03-31 16:32:10 -0400

    Fixed typo

--- db/createdb.sh
+++ db/createdb.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # script to create the db schema
 # by: Todd Burgess (toddburgess@torontocatrescue.ca)
-echo "Creating the Datasbase"
+echo "Creating the Database"
 mysql --user=root --password=root torontocatrescue < schema.sql
 echo "Database created. Now run populate.sh"
