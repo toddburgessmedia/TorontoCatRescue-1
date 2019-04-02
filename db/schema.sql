df996f62c488a729900e01c503404b0701ff0a8a
Todd Burgess <todd@toddburgessmedia.com>
2019-04-02 13:12:25 -0400

    Create database torontocatrescue if it does not exist

--- db/schema.sql
+++ db/schema.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `Cat`
 --
 
+CREATE DATABASE IF NOT EXISTS torontocatrescue;
+
 DROP TABLE IF EXISTS `Cat`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
